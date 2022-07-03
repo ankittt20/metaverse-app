bd75d883207342a6d7907f29a5c23f76db1be276
Ankit Kumar <ankitkumar3624@gmail.com>
2022-07-03 20:25:52 +0530

    final build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,13 @@ import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MoralisProvider
-      serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
-      appId={process.env.NEXT_PUBLIC_APP_ID}
-    >
-      <Component {...pageProps} />
-    </MoralisProvider>
+    <>
+      <MoralisProvider
+        serverUrl={`${process.env.NEXT_PUBLIC_SERVER_URL}`} appId={`${process.env.NEXT_PUBLIC_APP_ID}`}
+      >
+        <Component {...pageProps} />
+      </MoralisProvider>
+    </>
   );
 }
 
